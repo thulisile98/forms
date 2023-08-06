9f8fdfbff712ca0e90e208008a9019ca8e6a1d0c
Angel Mashego <thulisile.ma98@gmail.com>
2023-08-06 11:37:03 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
 
 
